30137fac76bd1a874d2a20f318aea1d429ed08a3
cnhkkat <1071541105@qq.com>
2022-10-28 00:07:11 +0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import store  from './store/store';
 import App from './App';
 import Home from './pages/home';
 
-// import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter } from 'react-router-dom';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -37,8 +35,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.log);
